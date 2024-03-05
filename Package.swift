31c06159e0750e1db88e32fe54bb1f52cbbfe0bb
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2024-03-05 16:44:38 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+/*
+ * Copyright (c) Au10tix.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "au10tixdetectormanager-ios",
+    products: [ 
+        .library(name: "Au10tixDetectorManager", targets: ["Au10tixDetectorManager"])
+    ],
+    targets: [
+        .binaryTarget(
+          name: "Au10tixDetectorManager",
+          url: "https://github.com/au10tixmobile/Au10tixDetectorManager/archive/refs/tags/4.0.0.zip",
+          checksum: "58741a60c387dd59de4d184db761d007d0ac88a9989b6dac66e1e6627f9d93c7"
+        )
+    ]
+)
